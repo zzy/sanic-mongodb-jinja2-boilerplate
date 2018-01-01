7452b0fa0ae0285886b8ecd47f718d24b8ee9380
Ouds <2558256956@qq.com>
2018-01-01 13:30:14 +0800

    -

--- server.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from budshome.settings import BH
-
-if __name__ == "__main__":
-    BH.config.LOGO = None
-    BH.run(host="0.0.0.0", workers=3, port=82, debug=False)
